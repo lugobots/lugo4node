320ee2584369c24bc4df91062c70a9d5f92fa30c
rubens21 <rubens21@gmail.com>
2023-01-09 16:33:44 -0700

    fixing dist files

--- example/rl/dist/src/rl/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RemoteControl = exports.Gym = void 0;
-var gym_1 = require("./gym");
-exports.Gym = gym_1.Gym;
-var remoteControl_1 = require("./remoteControl");
-exports.RemoteControl = remoteControl_1.RemoteControl;
